3f8dae26cf33546b1730237e101832a356bb4eb8
Khalid <17-34538-2@student.aiub.edu>
2020-10-27 21:22:51 +0600

    lab

---
+++ lab.1/lab.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            int a = 18;
+            int b = 6;
+            int d;
+            int c = a + b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+            a = -5 + 8 * 6;
+            Console.WriteLine(a);
+            b = (55 + 9) % 9;
+            Console.WriteLine(b);
+            c = 20 + -3 * 5 / 8;
+            Console.WriteLine(c);
+            d = 5 + 15 / 3 * 2 - 8 % 3;
+            Console.WriteLine(d);
+           double wide = 5.5;
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
