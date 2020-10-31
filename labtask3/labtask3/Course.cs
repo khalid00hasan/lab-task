79e28e3293f85c69d620eae013824582292aa1cc
Khalid <17-34538-2@student.aiub.edu>
2020-10-31 11:50:08 +0600

    labtask3

---
+++ labtask3/labtask3/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask3
+{
+    class Course
+    {
+        string courseName;
+        string courseCode;
+        int courseCredit;
+    }
+}
